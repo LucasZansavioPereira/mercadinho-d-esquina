796919edc0e6b3fbcadb4ae45977cf5480b7a7a9
Lucas <lmgjpereira@gmail.com>
2025-04-19 15:27:35 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -1,8 +1,8 @@
 CREATE TABLE dados (
-    AlunoID int,
+    ProdutoID int,
     Nome varchar(50),
-    Sobrenome varchar(50),
-    Endereco varchar(150),
-    Cidade varchar(50),
+    Origem varchar(50),
+    Quantidade varchar(150),
+    Validade int,
     Host varchar(50)
 );
